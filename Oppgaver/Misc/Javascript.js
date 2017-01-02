0154e1d5a15e40e16b9785de975b1dcdc7c867f1
oonray <your_email@example.com>
2017-01-02 14:01:04 +0100

    Oppgave 4 ferdig

--- Oppgaver/Misc/Javascript.js
+++ Oppgaver/Misc/Javascript.js
@@ -1,20 +1,45 @@
 console.log("Loaded");
 
+var head = "<title>Oppgaver</title>";
+head +='<link rel="stylesheet" type="text/css" href="Misc/css.css" />';
+head +='<link id="Css" rel="stylesheet" type="text/css" href="" />';
+head +='<meta charset="UTF-8"/>';
+
+document.head.innerHTML = head;
+
+var Body;
+var Link;
+
 var Body = document.getElementById("Body");
 var Link = document.getElementById("Css");
 
-var Home = '<p><h1>Velkommen!</h1><h3>Her finner du oppgavene</h3><p>For å velge oppgave klikk på klappene overnfor<br/> eller start på <a onclick="a('+'"oppgave_1"'+');">oppgave 1</a><br/>Filene til hver oppgave finner du under CSS eller HTML.<br/>Hvis du har spørsmål kontakt Læren.</p></p>';
+var Home = '<p><h1>Velkommen!</h1>';
+Home += "<h3>Her finner du oppgavene</h3>";
+Home += '<p>For å velge oppgave klikk på klappene overnfor<br/> eller start på <a onclick="a('+'"oppgave_1"'+');">oppgave 1</a>';
+Home += '<br/>Filene til hver oppgave finner du under CSS eller HTML.<br/>Hvis du har spørsmål kontakt Læren.</p></p>';
+
+function addlink(){
+    a = Body.getElementsByTagName("link");
+    for(var i=0;i<a.length();i++){
+        document.head.append(a[i]); 
+    };
+    
+};
 
 function H(){
     Body.innerHTML = Home;
 }
 
 function A(name){
-
+    document.head.innerHTML = head;
+   
+    Body = document.getElementById("Body");
+   
     var HTML = new XMLHttpRequest();
     HTML.onreadystatechange = function(){
             if(this.readyState == 4){
                 Body.innerHTML = this.responseText;
+                addlink()
             }}   
 
     HTML.open("GET","HTML/"+name+".html");
